563a902bf18510cba1fc9ccb0913d36acd3a2fb9
Steven Quintana <steve.e.quint@gmail.com>
2021-11-29 06:58:11 -0800

    Set up App routes to the pages

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import {
+  Home,
+  Sessions,
+  Preferences
+} from '../../pages'
 
 function App() {
   return (
-    <>
-      New Sesh
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='sessions'>
+          <Sessions />
+        </Route>
+        <Route path='preferences'>
+          <Preferences />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
