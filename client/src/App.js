4793cba1428a925abdf26fbeee90fed18a154ebe
Steven Quintana <steve.e.quint@gmail.com>
2021-12-01 16:36:27 -0800

    Create app component with page routes

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Home from './pages/Home/Home'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
